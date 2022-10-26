3d153f30d68a37e9574a7d236c7ae9c1350b8cab
jabali2004 <32802935+jabali2004@users.noreply.github.com>
2022-10-26 21:51:45 +0200

    feat: add openapi client gen #3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
+import React, { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Configuration } from './api';
+import { HealthApi } from './api/api/health-api';
 import './App.css';
 
 export default function App() {
+  const configuration = new Configuration({
+    basePath: 'http://localhost:3000',
+  });
+  const healthApi = new HealthApi(configuration);
+  const [health, setHealth] = useState('loading');
+
+  React.useEffect(() => {
+    healthApi.healthControllerCheck().then((res) => {
+      if (res.data.status) {
+        setHealth(res.data.status);
+      }
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<>Hello World</>} />
+      <Route path="/" element={<>Hello World, State: {health}</>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
