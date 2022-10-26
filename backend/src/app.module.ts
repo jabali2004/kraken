0e6e82a6f4eb7a66ab685f7a232b97134ea0088b
jabali2004 <32802935+jabali2004@users.noreply.github.com>
2022-10-27 00:17:52 +0200

    chore: configure cache #2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, CacheStore, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma/prisma.service';
@@ -7,13 +7,22 @@ import { TerminusModule } from '@nestjs/terminus';
 import { PrismaHealthIndicator } from './controllers/health/health.indicator';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { redisStore } from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TerminusModule,
     ThrottlerModule.forRoot({
       ttl: 10,
       limit: 25,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore as unknown as CacheStore,
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT || 6379,
     }),
   ],
   controllers: [AppController, HealthController],
